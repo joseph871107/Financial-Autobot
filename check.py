5717b969d9500ee0b5c54f0426bb7e308ebacd51
Joseph Ouyang <hsu.joseph.ouyang@gmail.com>
2023-12-02 01:54:07 +0800

    Fix unstaged changes

---
+++ check.py
@@ -0,0 +1,26 @@
+print("checking packages...")
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas
+import sklearn
+import keras
+import eli5
+import lxml
+import skater
+import talib
+import tensorflow
+import ipykernel
+import lightgbm
+import bottleneck
+import pyfolio
+import xgboost
+import shap
+import mlxtend
+import seaborn
+
+print("========================================")
+print("all packages are successfully installed!")
+print("    it's time to start the journey!")
+print("========================================")
